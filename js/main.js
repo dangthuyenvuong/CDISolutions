4c6bcbe15e8d9adcc5be8574d1521c37753667d8
Vuong Dang <vuongdang@chidoanh.com>
2021-08-15 10:53:05 +0700

    update

--- js/main.js
+++ js/main.js
@@ -1,47 +1,5 @@
-$(document).ready(() => {
-    $('#page-loading').hide()
-})
 
-$(window).on('scroll', () => {
-    if ($(window).scrollTop() > 0) {
-        $('#header').addClass('header-fixed')
-    } else {
-        $('#header').removeClass('header-fixed')
-    }
-})
-
-let $carousel = $('.main-carousel').flickity({
-    // options
-    autoPlay: true,
-    cellAlign: 'center',
-    contain: true,
-    wrapAround: true,
-})
-
-$carousel.find('.carousel-cell').on('click', function() {
-    let i = $(this).data('index')
-    $carousel.flickity('select', i)
-})
-
-// var flkty = $carousel.data('flickity');
-// let $imgs = $('.carousel-cell img');
-// $carousel.on('scroll.flickity', function (event, progress) {
-//     flkty.slides.forEach(function (slide, i) {
-//         var img = $imgs[i];
-//         var x = (slide.target + flkty.x) * -1 / 3;
-//         img.style.transform = 'translateX( ' + x + 'px)';
-//     });
-// });
-
-
-
-$('.carousel').flickity({
-    // options
-    autoPlay: true,
-    cellAlign: 'center',
-    contain: true,
-    wrapAround: true
-});
+// ------------ HEADER -----------------
 
 (() => {
     let $menu
@@ -54,13 +12,22 @@ $('.carousel').flickity({
 
         e.preventDefault()
         e.stopPropagation()
-        $('body').toggleClass('open-menu')
 
+        $menu = $(this).closest('li')
+        if ($menu.hasClass('open')) {
+            $('body').removeClass('open-menu')
+            $menu.removeClass('open')
+        } else {
+            $('#header .level1 li.open').removeClass('open')
+            $menu.addClass('open')
+            $('body').addClass('open-menu')
+
+        }
 
         // if ($menu && $menu?.[0] !== this) {
         //     $menu?.removeClass('open')
         // }
-        $menu = $(this).closest('li').toggleClass('open')
+
         // if ($menu.hasClass('open')) {
         //     $('body').addClass('open-menu')
         // } else {
@@ -78,18 +45,34 @@ $('.carousel').flickity({
 
     $('#overlay').on('click', closeMenu)
 
-    $('.services-col a').on('click', (e) => {
-        let id = $(e.currentTarget).data('id')
-        $('.services-col .active').removeClass('active')
-        $(e.currentTarget).addClass('active')
+    $('.services-col a').on('click', function () {
+        let $this = $(this)
 
-        $('.feature-col.active').removeClass('active').hide()
-        $(`.feature-col.${id}`).addClass('active').show()
+        let id = $this.data('id')
+
+        $this.closest('.services-col').find('.active').removeClass('active')
+        $this.addClass('active')
+
+        $this.closest('li').find('.feature-col.active').removeClass('active').hide()
+        $this.closest('li').find(`.feature-col.${id}`).addClass('active').show()
     })
+
+
+    $(window).on('scroll', () => {
+        if ($(window).scrollTop() > 0) {
+            $('#header').addClass('header-fixed')
+        } else {
+            $('#header').removeClass('header-fixed')
+        }
+    })
+
 })()
 
-const convertImages = (image, callback) => {
 
+
+
+//  ----------------------- Lazy loading image --------------------
+const convertImages = (image, callback) => {
     fetch(image.src)
         .then(res => res.text())
         .then(data => {
@@ -98,18 +81,64 @@ const convertImages = (image, callback) => {
 
             if (image.id) svg.id = image.id;
             if (image.className) svg.classList = image.classList;
+
             svg.removeAttribute('width')
             svg.removeAttribute('height')
 
+
             image.parentNode.replaceChild(svg, image);
+
+
         })
         .then(callback)
         .catch(error => console.error(error))
 }
-
 $('img[src$=".svg"],[data-src$=".svg"]').each((i, e) => {
     if (e.dataset.src) {
         e.src = e.dataset.src
     }
     convertImages(e)
 })
+
+
+
+$(document).ready(() => {
+    $('#page-loading').hide()
+
+
+    let $carousel = $('.main-carousel').flickity({
+        // options
+        autoPlay: true,
+        cellAlign: 'center',
+        contain: true,
+        wrapAround: true,
+    })
+
+    // $carousel.find('.carousel-cell').on('click', function () {
+    //     let i = $(this).data('index')
+    //     $carousel.flickity('select', i)
+    // })
+
+    // var flkty = $carousel.data('flickity');
+    // let $imgs = $('.carousel-cell img');
+    // $carousel.on('scroll.flickity', function (event, progress) {
+    //     flkty.slides.forEach(function (slide, i) {
+    //         var img = $imgs[i];
+    //         var x = (slide.target + flkty.x) * -1 / 3;
+    //         img.style.transform = 'translateX( ' + x + 'px)';
+    //     });
+    // });
+
+    $('.carousel').each((i, e) => {
+        if ($(e).find('> *').length > parseInt($(e).attr('data-carousel-min') || 1)) {
+            $(e).flickity({
+                // options
+                autoPlay: true,
+                cellAlign: 'center',
+                contain: true,
+                wrapAround: true
+            });
+        }
+    })
+})
+
